bc4ec9488891d434eb895f083cc5a92c07b2f307
Kumar Umang <umang7622@gmail.com>
2025-03-12 14:42:13 +0530

    Update index.js
    Final Piece

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ let port=8080;
 
 app.set("views",path.join(__dirname,"/views"));
 
-app.get("/",(req,res)=>{
-    res.render("home.ejs");
-});
-
-app.get("/rolldice",(req,res)=>{
-    let diceVal= Math.floor(Math.random() *6 +1);
-    res.render("rolldice.ejs",{num:diceVal});
-});
-
 app.listen(port,()=>{
     console.log(`App is listening on port ${port}`);
 });
@@ -23,4 +14,4 @@ app.get("/ig/:username",(req,res)=>{
     let {username}=req.params;
     const instaData=require("./data.json");
     res.render("instagram.ejs",{data:instaData[username]});
-})
+});
